e735754cbacf8ca950ff9b4e0048a22ee94eb654
DillyzThe1 <dyl1029an@gmail.com>
2022-12-17 00:37:10 -0600

    AYOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOO

--- IntroCutscenePatch.cs
+++ IntroCutscenePatch.cs
@@ -12,15 +12,27 @@ namespace DillyzRoleApi_Rewritten
     [HarmonyPatch(typeof(IntroCutscene._ShowRole_d__35), nameof(IntroCutscene._ShowRole_d__35.MoveNext))]
     class IntroCutscenePatch
     {
+        public static string colorHex = "#FF00000";
         public static void Postfix(IntroCutscene._ShowRole_d__35 __instance) {
             CustomRole role = CustomRole.getByName(DillyzUtil.getRoleName(PlayerControl.LocalPlayer));
             if (role == null)
                 return;
-            string colorHex = DillyzUtil.colorToHex(role.roleColor);
+            colorHex = DillyzUtil.colorToHex(role.roleColor);
             HarmonyMain.Instance.Log.LogInfo(role.roleColor + " " + colorHex);
             __instance.__4__this.RoleText.text = $"<{colorHex}>{role.name}</color>";
             __instance.__4__this.RoleBlurbText.text = $"<{colorHex}>{role.subtext}</color>";
             __instance.__4__this.YouAreText.text = $"<{colorHex}>Your role is</color>";
+        }
+    }
+
+    // force it to update
+    [HarmonyPatch(typeof(TextMeshPro), nameof(TextMeshPro.InternalUpdate))]
+    class TMPPROPatch
+    {
+        public static void Postfix(TextMeshPro __instance)
+        {
+            if (__instance.name == "YouAreText")
+                __instance.text = $"<{IntroCutscenePatch.colorHex}>Your role is</color>";
         }
     }
 }
