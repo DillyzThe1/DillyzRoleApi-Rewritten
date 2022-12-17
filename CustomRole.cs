f3e42c14b7663aca2422a51171f51580bd294464
DillyzThe1 <dyl1029an@gmail.com>
2022-12-16 18:51:48 -0600

    i'm going insane but here is a role thing idk

---
+++ CustomRole.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+// the idea here is that you'll override this class for your own roles
+// example for overriding a field: public String name => "Jester";
+namespace DillyzRoleApi_Rewritten
+{
+    class CustomRole
+    {
+        public String name { get; set; }             // Your role's name.
+        public String subtext { get; set; }          // The text that appears under.
+        public bool nameColorChanges { get; set; }   // Determines if your name color is your role color or just red/white.
+        public Color roleColor { get; set; }         // The current color of your role.
+        public bool teamCanSeeYou { get; set; }      // Determines if your team can see you.
+        public CustomRoleSide side { get; set; }     // Determines who you work with.
+        public bool commitsTaxFraud { get; set; }    // 192.512.3.62
+        public bool canVent { get; set; }            // If you can use the vents or not. You're an idiot for reading this text.
+        public bool canKill { get; set; }            // Are you seriously this blind?
+    }
+
+    internal enum CustomRoleSide { 
+        Impostor = 0,     // You work alongside the Impostors.
+        Crewmate = 1,     // You work alongside the Crewmates.
+        Independent = 2,  // You work upon your own team.
+        LoneWolf = 3      // You work by yourself.
+    }
+}
