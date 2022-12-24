ef4eb5078a15ea0781e379d7b9ad84ba7458d0ae
DillyzThe1 <dyl1029an@gmail.com>
2022-12-23 23:45:20 -0600

    make sure to not murder lol

---
+++ PlayerControlPatch.cs
@@ -0,0 +1,22 @@
+﻿using HarmonyLib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DillyzRoleApi_Rewritten
+{
+    class PlayerControlPatch
+    {
+        [HarmonyPatch(typeof(PlayerControl), nameof(PlayerControl.MurderPlayer))]
+        public class PlayerControlPatch_MurderPlayer {
+            public static bool Prefix(PlayerControl __instance, PlayerControl target) {
+                HarmonyMain.Instance.Log.LogWarning($"{__instance.name} tried to kill {target.name} using the normal PlayerControl.MurderPlayer() function!" +
+                                                                                                $" (Did you forget to do DillyzUtil.RpcCommitAssassination()?)");
+                DillyzUtil.commitAssassination(__instance, target);
+                return false;
+            }
+        }
+    }
+}
