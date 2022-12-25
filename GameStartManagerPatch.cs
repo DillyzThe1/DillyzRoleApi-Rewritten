3f38ed8c4618acca28e58dc9f436234b008b2959
DillyzThe1 <dyl1029an@gmail.com>
2022-12-24 23:59:22 -0600

    no more public lobbies

---
+++ GameStartManagerPatch.cs
@@ -0,0 +1,18 @@
+﻿using HarmonyLib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace DillyzRoleApi_Rewritten
+{
+    [HarmonyPatch(typeof(GameStartManager), nameof(GameStartManager.Update))]
+    class GameStartManagerPatch
+    {
+        public static void Postfix(GameStartManager __instance) {
+            __instance.MakePublicButton.gameObject.SetActive(false);
+        }
+    }
+}
