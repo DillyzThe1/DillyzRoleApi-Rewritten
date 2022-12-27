a4d1f069b934783a54ef7cb31e2f557d309c2b3e
DillyzThe1 <dyl1029an@gmail.com>
2022-12-27 11:02:10 -0600

    role prank

---
+++ ShipStatusPatch.cs
@@ -0,0 +1,15 @@
+﻿using HarmonyLib;
+
+namespace DillyzRoleApi_Rewritten
+{
+    [HarmonyPatch(typeof(ShipStatus), nameof(ShipStatus.OnDestroy))]
+    class ShipStatusPatch
+    {
+        public static void Prefix(ShipStatus __instance) {
+            //if (DillyzUtil.InGame() && AmongUsClient.Instance.GameState != InnerNet.InnerNetClient.GameStates.Ended)
+            //CustomRole.allRoleNames.Clear();
+            if (DillyzUtil.InFreeplay())
+                CustomRole.setRoleName(PlayerControl.LocalPlayer.PlayerId, "");
+        }
+    }
+}
