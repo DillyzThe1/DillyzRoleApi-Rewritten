9fdc4ef571d5a22b320ce1c7cc5511dbee750729
DillyzThe1 <dyl1029an@gmail.com>
2023-01-01 01:43:57 -0600

    no more

---
+++ GameModeMenuPatch.cs
@@ -0,0 +1,20 @@
+﻿using HarmonyLib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DillyzRoleApi_Rewritten
+{
+    class GameModeMenuPatch
+    {
+        [HarmonyPatch(typeof(GameModeMenu), nameof(GameModeMenu.OnEnable))]
+        public class GameModeMenuPatch_OnEnable {
+            public static void Postfix(GameModeMenu __instance) {
+                __instance.transform.parent.gameObject.SetActive(false);
+                __instance.ChooseOption(AmongUs.GameOptions.GameModes.Normal);
+            }
+        }
+    }
+}
