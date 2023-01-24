0b9ed05aab2257a34bd118cb41d6d6713c0441cb
DillyzThe1 <dyl1029an@gmail.com>
2023-01-23 22:31:11 -0600

    funny freeplay pages

--- TaskAddMinigamePatch.cs
+++ TaskAddMinigamePatch.cs
@@ -9,13 +9,64 @@ using static DillyzRoleApi_Rewritten.PlayerControlPatch;
 
 namespace DillyzRoleApi_Rewritten
 {
-    [HarmonyPatch(typeof(TaskAddButton), nameof(TaskAddButton.Update))]
-    class TaskAddButtonPatch_Update
+    [HarmonyPatch(typeof(TaskAdderGame), nameof(TaskAdderGame.OnEnable))]
+    class TaskAddMinigamePatchStart
     {
-        public static void Postfix(TaskAddButton __instance)
-        { 
-            if (__instance.Role != null && TaskAddMinigamePatch.customTime)
-                __instance.Overlay.gameObject.GetComponent<SpriteRenderer>().enabled = false;
+        public static GameObject bback;
+        public static GameObject bfoward;
+        public static void Postfix(TaskAdderGame __instance)
+        {
+            TaskAddMinigamePatch.lastInstance = __instance;
+            DillyzRoleApiMain.Instance.Log.LogInfo("women :coffee:");
+
+            Transform closeTrans = __instance.gameObject.transform.Find("CloseButton");
+
+
+            __instance.gameObject.transform.Find("TitleText_TMP").gameObject.SetActive(false);
+
+            float dist = -0.575f;
+
+            // DOWN
+            bback = GameObject.Instantiate(closeTrans.gameObject);
+            bback.name = "ButtonDir_Back";
+            bback.transform.parent = closeTrans.parent;
+            bback.transform.localPosition += new Vector3(0f, dist, -100f);
+            bback.GetComponent<SpriteRenderer>().sprite = DillyzUtil.getSprite(Assembly.GetExecutingAssembly(), "DillyzRoleApi_Rewritten.Assets.arrowbuttonleft.png");
+
+            PassiveButton bback_pb = bback.GetComponent<PassiveButton>();
+
+            if (bback_pb != null)
+            {
+                ControllerManager.Instance.AddSelectableUiElement(bback_pb, false);
+                bback_pb.OnClick = new UnityEngine.UI.Button.ButtonClickedEvent();
+                bback_pb.OnClick.AddListener((UnityAction)callback);
+                void callback()
+                {
+                    TaskAddMinigamePatch.pageIndex--;
+                    TaskAddMinigamePatch.ReposAllItems();
+                }
+            }
+
+            // UP
+            bfoward = GameObject.Instantiate(closeTrans.gameObject);
+            bfoward.name = "ButtonDir_Foward";
+            bfoward.transform.parent = closeTrans.parent;
+            bfoward.transform.localPosition += new Vector3(0f, dist*2, -100f);
+            bfoward.GetComponent<SpriteRenderer>().sprite = DillyzUtil.getSprite(Assembly.GetExecutingAssembly(), "DillyzRoleApi_Rewritten.Assets.arrowbuttonright.png");
+
+            PassiveButton bfoward_pb = bfoward.GetComponent<PassiveButton>();
+
+            if (bback_pb != null)
+            {
+                ControllerManager.Instance.AddSelectableUiElement(bfoward_pb, false);
+                bfoward_pb.OnClick = new UnityEngine.UI.Button.ButtonClickedEvent();
+                bfoward_pb.OnClick.AddListener((UnityAction)callback2);
+                void callback2()
+                {
+                    TaskAddMinigamePatch.pageIndex++;
+                    TaskAddMinigamePatch.ReposAllItems();
+                }
+            }
         }
     }
     [HarmonyPatch(typeof(TaskAdderGame), nameof(TaskAdderGame.ShowFolder))]
@@ -23,7 +74,11 @@ namespace DillyzRoleApi_Rewritten
     {
         public static bool customTime = false;
         public static int pageCount = 0, pageIndex = 0;
-        public static void Postfix(TaskAdderGame __instance, TaskFolder taskFolder) {
+        public static TaskAdderGame lastInstance;
+
+        public static void Postfix(TaskAdderGame __instance, TaskFolder taskFolder)
+        {
+            TaskAddMinigamePatch.lastInstance = __instance;
             if (__instance.Hierarchy.Count != 1)
                 return;
 
@@ -40,7 +95,8 @@ namespace DillyzRoleApi_Rewritten
             DillyzRoleApiMain.Instance.Log.LogInfo("base pos [" + xc + ", " + yc + "]");
             DillyzRoleApiMain.Instance.Log.LogInfo("max pos x " + (__instance.lineWidth));
 
-            foreach (CustomRole role in CustomRole.allRoles) {
+            foreach (CustomRole role in CustomRole.allRoles)
+            {
                 if (role.hiddenFromFreeplay)
                     continue;
 
@@ -94,7 +150,7 @@ namespace DillyzRoleApi_Rewritten
                             TaskAddButton taskadd = trans.gameObject.GetComponent<TaskAddButton>();
                             if (taskadd == null || taskadd.Overlay == null)
                                 continue;
-                            
+
                             taskadd.Overlay.gameObject.GetComponent<SpriteRenderer>().enabled = false;
                         }
                         sprrend.enabled = true;
@@ -129,9 +185,35 @@ namespace DillyzRoleApi_Rewritten
                 }
                 __instance.PathText.text = stringBuilder.ToString() + " (" + (pageIndex + 1) + "/" + pageCount + ")";
             }
+
+            if (TaskAddMinigamePatchStart.bback != null)
+                TaskAddMinigamePatchStart.bback.gameObject.SetActive(pageCount > 1);
+            if (TaskAddMinigamePatchStart.bfoward != null)
+                TaskAddMinigamePatchStart.bfoward.gameObject.SetActive(pageCount > 1);
+
+            ReposAllItems();
         }
 
-        public static void the(TaskAdderGame __instance, RoleBehaviour arbys, List<TaskAddButton> buttons) {
+        public static void ReposAllItems() {
+            if (lastInstance == null)
+                return;
+
+            if (TaskAddMinigamePatch.pageIndex < 0)
+                TaskAddMinigamePatch.pageIndex = 0;
+            if (TaskAddMinigamePatch.pageIndex >= pageCount)
+                TaskAddMinigamePatch.pageIndex = pageCount - 1;
+
+            float filedist = 1.2f;
+
+            for (int i = 0; i < lastInstance.ActiveItems.Count; i++) {
+                Transform trans = lastInstance.ActiveItems[i];
+                trans.localPosition = new Vector3(trans.localPosition.x, ((-filedist)*Mathf.RoundToInt(Mathf.Floor(i/5f)) + ((filedist*5f)*((float)pageIndex))) + 0.55f, trans.localPosition.z);
+                trans.gameObject.SetActive(pageIndex == (Mathf.RoundToInt(Mathf.Ceil((i + 1) / 25f)) - 1));
+            }
+        }
+
+        public static void the(TaskAdderGame __instance, RoleBehaviour arbys, List<TaskAddButton> buttons)
+        {
             foreach (Transform trans in __instance.ActiveItems)
             {
                 if (trans.gameObject == null)
@@ -144,7 +226,8 @@ namespace DillyzRoleApi_Rewritten
                     continue;
 
                 taskadd.Button.OnClick.AddListener((UnityAction)callback);
-                void callback() {
+                void callback()
+                {
                     taskadd.Overlay.gameObject.GetComponent<SpriteRenderer>().enabled = true;
                     CustomRole.setRoleName(PlayerControl.LocalPlayer.PlayerId, "");
                     customTime = false;
@@ -180,6 +263,16 @@ namespace DillyzRoleApi_Rewritten
                 taskadd.FileImage.color = newColor;
                 return;
             }
+        }
+    }
+    
+    [HarmonyPatch(typeof(TaskAddButton), nameof(TaskAddButton.Update))]
+    class TaskAddButtonPatch_Update
+    {
+        public static void Postfix(TaskAddButton __instance)
+        {
+            if (__instance.Role != null && TaskAddMinigamePatch.customTime)
+                __instance.Overlay.gameObject.GetComponent<SpriteRenderer>().enabled = false;
         }
     }
 }
